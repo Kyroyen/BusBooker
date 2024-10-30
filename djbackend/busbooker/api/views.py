7c8c66d26decc0ca11d43bbd9f992b6e4affc1c4
Rishu <rishush804@gmail.com>
2024-10-30 14:44:23 +0530

    final, now frontend

--- djbackend/busbooker/api/views.py
+++ djbackend/busbooker/api/views.py
@@ -1,10 +1,5 @@
-from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import BaseAuthentication
-from rest_framework.exceptions import AuthenticationFailed
 from django.contrib.auth.models import User
 from django.core.cache import cache
 
